06e7c2ddc298b18d06513448711c13786572b6fb
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-11-02 20:51:15 +0530

    signup done but not encrypted password

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,12 +1,25 @@
 import { Request, Response } from "express";
-
+import cloudinary from "../utils/cloudinary";
+import User from "../models/user.model";
 export const handleSignUp = async (req: Request, res: Response) => {
     try {
         const data = req.body;
         const profilePicture = req.file;
 
-        // console.log('Form data:', data);
-        console.log('File:', profilePicture);
+        const result = await cloudinary.uploader.upload(profilePicture!.path)
+
+        const user = new User({
+            name: data.name,
+            username: data.username,
+            email: data.email,
+            password: data.password,
+            profilePicture: result.secure_url,
+            role: data.role,
+        })
+        console.log(user);
+
+
+        await user.save()
 
         res.status(200).json({ message: 'Signup successful' });
     } catch (error) {
---
+++ backend/src/utils/cloudinary.ts
@@ -0,0 +1,22 @@
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+interface cloudinaryConfig {
+    cloud_name: string;
+    api_key: string;
+    api_secret: string;
+}
+
+if (!process.env.CLOUD_NAME || !process.env.API_KEY || process.env.API_SECRET) {
+    console.log("Cloudinary Credentials error");
+}
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME!,
+    api_key: process.env.API_KEY!,
+    api_secret: process.env.API_SECRET!,
+} as cloudinaryConfig)
+
+export default cloudinary
