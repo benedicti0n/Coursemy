f1ad49c42f2b736d11624c82fadabd98534aeb3b
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-11-10 20:03:20 +0530

    becomeCreator feature done

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { verifyToken } from "../middlewares/login.middleware";
-import { fetchProfileDetails } from "../controllers/profile.controller";
+import { fetchProfileDetails, becomeCreator } from "../controllers/profile.controller";
 
 const router = express.Router()
 
 router.get("/profile", verifyToken, fetchProfileDetails)
+router.post("/profile", verifyToken, becomeCreator)
 
 export default router
