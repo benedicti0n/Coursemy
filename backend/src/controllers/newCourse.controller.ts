03c3e20b7c786d8e4ec618211f2d621037c631eb
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-11-11 15:20:04 +0530

    newCourse controller

---
+++ backend/src/controllers/newCourse.controller.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express"
+import Course from "../models/course.model"
+import cloudinary from "../utils/cloudinary"
+
+const createCourse = async (req: Request, res: Response) => {
+    try {
+        const formData = req.body
+        const bannerPicture = req.file
+        const userId = req._id
+
+        const result = await cloudinary.uploader.upload(bannerPicture!.path)
+        const bannerPictureUrl = result.secure_url
+
+        const course = new Course({
+            name: formData.name,
+            bannerPicture: bannerPictureUrl,
+            description: formData.description,
+            price: formData.price,
+            content: formData.content,
+            createdBy: userId
+        })
+
+        await course.save()
+
+        console.log("Course created successfully");
+
+        res.status(200).json({ message: "Course created successfully" })
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export default createCourse
