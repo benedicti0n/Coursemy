c41684483f439bf132d57d79b3d74f22fdbec4a6
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-11-13 18:02:53 +0530

    minor change

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { verifyToken } from "../middlewares/login.middleware";
+import verifyToken from "../middlewares/login.middleware";
 import { fetchProfileDetails, becomeCreator } from "../controllers/profile.controller";
 
 const router = express.Router()
