4282464037741f67a62adcc89d9e5eaaa081e1b8
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-11-27 05:49:38 +0530

    multer file limit and correct dest

--- backend/src/routes/course.routes.ts
+++ backend/src/routes/course.routes.ts
@@ -5,7 +5,12 @@ import verifyToken from "../middlewares/login.middleware"
 import { createCourse, getAllCourses, fetchCourseDetails, buyCourse } from "../controllers/course.controller"
 
 const router = Router()
-const upload = multer({ dest: "uploads/" })
+const upload = multer({
+    dest: "/temp/uploads/",
+    limits: {
+        fileSize: 5 * 1024 * 1024
+    }
+})
 
 router.post("/createCourse", upload.single("bannerPicture"), verifyToken, isCreator, createCourse)
 router.get("/feed", getAllCourses)
