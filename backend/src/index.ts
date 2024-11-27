d9d5e4ec6be8b3466102a7cf071f6254360e8732
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-11-27 05:56:30 +0530

    idk

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,13 +20,17 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }))
+
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+    res.header('Access-Control-Allow-Origin', process.env.NODE_ENV === 'production'
+        ? 'https://coursemy-phi.vercel.app'
+        : 'http://localhost:5173');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
