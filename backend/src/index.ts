2154a1fbfc7901de8fe0f5a70131355462e38013
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-11-27 05:49:22 +0530

    security changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,6 +20,13 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
